4d8d3336a89d5dd253f97e73a53af890a13ce8ae
mmaiolinoc90 <mmaiolinoc@gmail.com>
2017-05-09 20:53:24 -0400

    parte 7
    Gestion eventos

---
+++ gestion_evento_externo.php
@@ -0,0 +1,236 @@
+<?php
+// Este es el id del usuario
+$id_usuario_externo = $_GET['user'];
+//tomamos los datos del archivo conexion.php  
+include("clases/conexion.php");
+$conexion = mysql_connect($host,$user,$ps)or die("problema al conectar el host");
+mysql_select_db($bd,$conexion)or die ("problemas al conectar la bd tabla usuario");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- JS que permite hacer el efecto en el radio button de institución aleada -->
+<script type="text/javascript">
+    var checkActive = false;
+    $(document).ready(function() {
+        $('#checkbox').change(function() {
+            if(checkActive == false) {
+                $('#institucion').css('display', 'block');
+                checkActive = true;
+            } else {
+                $('#institucion').css('display', 'none');
+                checkActive = false;
+            }
+        });
+    }); 
+
+	function mostrar_institucion()
+	{
+		document.getElementById('institucion').style.display='block';
+	}
+</script> 
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Gestión Evento Externo</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Theme CSS -->
+    <link href="css/agency.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY" crossorigin="anonymous"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo" crossorigin="anonymous"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Header de la pagina -->
+    <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top" style="background:white">
+        <div class="container"> 
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll"> <a href="index.php"> <img src="img/Logo_trans1.png" width="168" height="70"/></a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">  </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    
+                    <li>
+                        <a class="page-scroll" style="background:orange;" href="index.php">Volver</a>
+                    </li>
+                    <li>
+                       	 <?php //inicio PHP	
+                    			//se envia la consulta  
+								$nombre = mysql_query('SELECT * FROM usuario_externo WHERE correo="'.mysql_real_escape_string($id_usuario_externo).'"');
+								while ($row = mysql_fetch_row($nombre)){
+						?>
+                       		 <a class="page-scroll" style="background:orange; margin-left: 5%;"><?php echo $row[1].' '.$row[2] ?></a>
+						<?php
+								}
+						?>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav><!-- Fin Header de la pagina -->
+
+
+
+    <!-- Sección Formulario de Evento Interno -->
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Formulario Evento Externo</h2>
+                    <h3 class="section-subheading text-muted"></h3>
+                </div>
+            </div>
+            <div class="row">
+               <!-- formulario de datos personales -->
+                <div class="col-lg-12"> 
+                    <form name="formulario_registro" action="clases/resgistro_evento_externo.php" method="post" enctype="multipart/form-data" novalidate>
+                        <div class="row">    
+                            <div class="col-md-6">
+                              <h3 style="margin-left: 20%; color: white; font-size: 30px; font-weight: bold;">Datos Personales</h3>
+                               <!-- modulo PHP que trae los datos del usuario -->
+                               <?php //inicio PHP	
+                    			//se envia la consulta  
+								$nombre = mysql_query('SELECT * FROM usuario_externo WHERE correo="'.mysql_real_escape_string($id_usuario_externo).'"');
+								
+								//se inicia el ciclo que nos permite pbtener los valores del ususario.
+                               	while ($row = mysql_fetch_row($nombre)){
+								
+                                echo '<div class="form-group">'; 
+                                    echo '<input type="text" value="'.$row[1].'" class="form-control" disabled id="name" name="nombre">';
+                                   echo '<p class="help-block text-danger"></p>';
+                               echo '</div>';
+                                echo '<div class="form-group">';
+                                    echo '<input type="text" value="'.$row[2].'" class="form-control" disabled name="apellido" id="apellido">';
+                                   echo '<p class="help-block text-danger"></p>';
+                               echo '</div>';
+									echo '<div class="form-group">';
+                                    echo '<input type="text" value="'.$row[3].'" class="form-control" disabled name="cedula" id="cedula">';
+                                   echo '<p class="help-block text-danger"></p>';
+                               echo '</div>';
+                                echo '<div class="form-group">';
+                                   echo '<input type="email" value="'.$row[4].'" disabled class="form-control" id="email" name="email">';
+                                    echo '<p class="help-block text-danger"></p>';
+                                echo '</div>';	
+								echo '<div class="form-group">';
+                                   echo '<input type="tel" value="'.$row[5].'" disabled class="form-control" id="telefono" name="telefono">';
+                                    echo '<p class="help-block text-danger"></p>';
+                                echo '</div>';
+								echo '<div class="form-group">';
+                                   echo '<input value="'.$row[6].'" disabled class="form-control" id="empresa" name="empresa">';
+                                    echo '<p class="help-block text-danger"></p>';
+                                echo '</div>';
+								echo '<div class="form-group">';
+                                   echo '<input value="'.$row[7].'" disabled class="form-control" id="cargo" name="cargo">';
+                                    echo '<p class="help-block text-danger"></p>';
+                                echo '</div>';
+                               
+									echo '<input style="visibility:hidden" type="text" value="'.$row[0].'"  id="id_usuario" name="usu">';
+								}
+								?>
+                           
+                            </div>
+                            <!-- Final del formulario de datos personales -->
+                            
+                            <!-- formulario de datos del evento -->
+                            <div class="col-md-6">
+                               <h3 style="margin-left: 20%; color: white; font-size: 30px; font-weight: bold;">Datos del Evento</h3>
+                               <div class="form-group">
+                                    <input type="text"  class="form-control" id="name" name="titulo" placeholder="Escriba el nombre del evento" required data-validation-required-message="Debe escribir el nombre de su evento" required/>
+                                   <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text"  class="form-control" id="name" name="cantidad" placeholder="Escriba la cantidad de personas que asistirá a su evento" required data-validation-required-message="Debe escribir la cantidad de personas">  
+                                   <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text"  class="form-control" id="name" name="nro_bloque" placeholder="Escriba la cantidad de bloques que tendrá su evento" required data-validation-required-message="Debe escribir la cantidad de bloques de su evento">  
+                                   <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <textarea style="height: 147px;" class="form-control" placeholder="Escriba la descripción de su evento" id="descripcion" required data-validation-required-message="Debe escribir una descripcion del evento" name="descripcion"></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                   	<h5 style="color: white;">Adjunte el poster de su evento</h5>
+                                    <input name="foto" type="file"  class="form-control">  
+                                   <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                  	<h5 style="color: white; float: left;">¿Requiere Entradas para el evento? </h5>
+                                   	<input style="float: left; width: 30px; height: 30px;" name="entradas" id="entradas"  type="checkbox" value="SI" />
+                                </div>  
+
+                            </div >
+                            <!-- Final del formulario de datos del evento -->
+                            
+                            <div class="clearfix"></div>
+                            <div class="col-lg-12 text-center">
+                                <div id="success"></div>
+                                <button style="margin-top: 5%;" type="submit" class="btn btn-xl">Siguiente</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+	<!-- FIn gestion evento interno -->
+   
+   <?php
+		include("footer.php");
+	?>
+   
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/agency.min.js"></script>
+
+</body>
+
+</html>
+
---
+++ gestion_evento_interno_bloques.php
@@ -0,0 +1,284 @@
+<?php
+// Este es el id del usuario
+$id_usuario_interno = $_GET['query'];
+//tomamos los datos del archivo conexion.php  
+include("clases/conexion.php");
+$conexion = mysql_connect($host,$user,$ps)or die("problema al conectar el host");
+mysql_select_db($bd,$conexion)or die ("problemas al conectar la bd tabla usuario");
+
+$fecha_sistema = $_GET['query'];
+$nro_bloques = $_GET['bloques'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Gestión Evento Interno Sesiones</title>
+
+<link href="jquery.datetimepicker.css" rel="stylesheet">
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="jquery.datetimepicker.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href=
+    'https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Theme CSS -->
+    <link href="css/agency.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY" crossorigin="anonymous"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo" crossorigin="anonymous"></script>
+    <![endif]-->
+    
+
+
+<body id="page-top" class="index">
+
+    <!-- Header de la pagina -->
+    <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top" style="background:white">
+        <div class="container"> 
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll"> <a href="index.php"> <img src="img/Logo_trans1.png" width="168" height="70"/></a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">  </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    
+                    <li>
+                        <a class="page-scroll" style="background:orange;" href="index.php">Volver</a>
+                    </li>
+                    <li>
+                       	 <?php //inicio PHP	
+                    			//se envia la consulta  
+								//se hace consulta para traer ID_evento  
+								$nombre = mysql_query('SELECT usuario_interno_id_usuario_interno FROM evento_interno WHERE fecha_solicitud="'.mysql_real_escape_string($fecha_sistema).'"');
+						
+								
+								
+								while ($row = mysql_fetch_row($nombre)){
+									
+									$usu = mysql_query('SELECT * FROM usuario_interno WHERE id_usuario_interno="'.mysql_real_escape_string($row[0]).'"');
+									
+									while ($row_nombre = mysql_fetch_row($usu))
+									{
+						?>
+                       		 <a class="page-scroll" style="background:orange; margin-left: 5%;"><?php echo $row_nombre[1].' '.$row_nombre[2] ?></a>
+						<?php
+									}
+								}
+						?>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav><!-- Fin Header de la pagina -->
+
+
+
+    <!-- Sección datos de la sesion -->
+    <section id="contact" style="background-repeat: repeat; background-position:top;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Formulario Evento Interno</h2>
+                    <h3 class="section-subheading text-muted"></h3>
+                </div>
+            </div>
+            <form name="sesiones" action="clases/registro_evento_interno_horas.php" enctype="multipart/form-data" method="post"  novalidate>
+             <?php 
+						$contador_fechas_Jquery = 0;
+							//se inicia el ciclo FOR que va a permitir que se puedan mostrar todos los bloques requeridos
+						for ($contador_bloques =  1; $contador_bloques <= $nro_bloques; $contador_bloques++)
+						{
+									$contador_fechas_Jquery++;	
+			?>
+           
+              <div class="row">
+                <div class="col-lg-12">
+                        <div class="row">
+                            <div class="col-md-6">
+                               <h3 style="margin-left: 17%; color: white; font-size: 30px; font-weight: bold;">Datos de la sesión <?php echo $contador_bloques ?></h3>
+                                <div class="form-group">
+                                    <input name="fecha<?php echo $contador_bloques?>" type="text" id="datetimepicker" class="form-control">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <input name="Hora_inicio<?php echo $contador_bloques?>" type="time" class="form-control">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <input name="Hora_final<?php echo $contador_bloques?>" type="time" class="form-control" >
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <?php 
+                                echo '<div class="form-group">';
+		
+										$conexion = mysql_connect($host,$user,$ps)or die("problema al conectar el host");
+										mysql_select_db($bd,$conexion)or die ("problemas al conectar la bd tabla usuario");
+										//se envia la consulta 
+										$id_localidad = mysql_query('SELECT id_localidad FROM localidad');
+										// consulta para traer nombre de la localidad
+										 	
+                                    echo '<select name="localidad'.$contador_bloques.'" class="form-control" style="width:100%; height:60px; margin-bottom:20px;  ">';
+									
+									while ( $row9 =mysql_fetch_array($id_localidad) )   
+										{
+									?>
+											<option  value=" <?php echo $row9['id_localidad'] ?> " >
+												<?php  $row9['id_localidad']; 
+														$localidad = mysql_query('SELECT nombre FROM localidad WHERE id_localidad="'.mysql_real_escape_string($row9['id_localidad']).'"');
+														 while ( $row10 =mysql_fetch_array($localidad))
+													   	   {
+														   	echo $row10['nombre'];
+														   }
+											?>
+														
+												</option>
+											
+									<?php		
+										}
+									echo '</select>';
+                                    echo '<p class="help-block text-danger"></p>';
+                               echo '</div>';
+								echo '<input type="text"  name="nro_bloques" value="'.$nro_bloques.'?>" style="visibility:hidden" />';
+								
+								//se hace consulta para traer ID_evento  
+							$resultado1 = mysql_query('SELECT * FROM evento_interno WHERE fecha_solicitud="'.mysql_real_escape_string($fecha_sistema).'"'); 
+							//
+							 
+							while ($row = mysql_fetch_row($resultado1))
+							{
+								echo ' <table   style="margin-left:70px;"> ';  //comienza la tabla de los bloques
+										echo '<tr>';
+										 echo '<td><input value="'.$row[0].'" name="id_evento" type="text" style="visibility:hidden" /></td>';
+										 echo '<td><input value="'.$row[9].'" name="id_usuario_interno" type="text" style="visibility:hidden" /></td>';
+											echo "</tr>";
+										echo "</table>";
+							}
+								
+								?>
+                            </div>
+                            <div class="col-md-6" style=" height: 430px; overflow: auto;">
+                              
+                               
+                                 <h3 style="margin-left: 12%; color:white;  font-size: 30px; font-weight: bold;">Servicios de la sesión <?php echo $contador_bloques ?></h3>
+
+									<div style=" width: 100%; background: white; height: auto; border-radius: 5px;">
+										
+										<div style="background: white; height: auto; margin-bottom: 5%; float:left;">	
+											<?php   
+										   // En este PHP se traen los se rvicios de la BD.
+
+												$contador_servicios = 0;
+												//se envia la consulta  para traer los tipos de servicios
+												$id_tipo_servicios = mysql_query('SELECT * FROM tipo_servicio');
+												// ciclo par amostrar los tipos de servicios
+
+												//Ciclo para mostrar los servicios
+												while ( $row_tipo_servicios = mysql_fetch_array($id_tipo_servicios) )
+												{
+														echo '<div style=" width: 100%; height: auto;">';
+															echo '<h5 style="color: black; margin-left:3%; font-weight: bold;">'.$row_tipo_servicios[1].'</h5>';
+														echo '</div>';
+
+														//se envia la consulta para traer servicios 
+														$id_servicios = mysql_query('SELECT * FROM servicios WHERE id_tipo_servicio = "'.$row_tipo_servicios[0].'"');
+
+
+															echo '<div style=" float: left; width: 100%; margin-bottom: 5%; height: auto;">';
+
+															while ( $row_servicios =mysql_fetch_array($id_servicios) )
+														{
+																	echo '<div style=" float: left; margin-left: 6%; width: 5%;">';
+																		echo '<input style="width: 20px; height: 20px; margin-left: 15%; margin-top: 30%;" name="checkbox'.$contador_bloques.'[]" type="checkbox" value="'.$row_servicios[0].'"/>';
+																	echo '</div>';
+
+																	echo '<div style="color:black; float: left; width: 35%;">';
+																			echo '<h6>'.$row_servicios[1].'</h6>';
+																	echo '</div>';
+
+														}
+															echo '</div>';
+													}
+												?>
+											</div>
+									</div>
+		
+                                    <textarea style="height: 147px; " class="form-control" name="observaciones<?php echo $contador_bloques ?>" placeholder="Datos complementarios del evento" id="observaciones" required data-validation-required-message="Datos Complementarios del evento"></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                	</div>
+                	<?php
+							
+							}//cierra el FOR
+					?> 
+               		<div class="col-lg-12 text-center">
+                                <div id="success"></div>
+                                <button type="submit" class="btn btn-xl">Siguiente</button>
+                    </div>
+                </form>
+                 
+                
+            </div>
+        </div>
+    </section>
+	<!-- FIn datos de la sesion -->
+   
+   	<!-- Sección Pie de pagina -->
+    <?php
+		include("footer.php");
+	?>
+	<!-- FIN Sección Pie de pagina -->
+   
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/agency.min.js"></script>
+<script src="js/jquery.datetimepicker.full.js"></script>
+<script>
+		$("#datetimepicker").datetimepicker();
+</script>
+
+</body>
+
+</html>
